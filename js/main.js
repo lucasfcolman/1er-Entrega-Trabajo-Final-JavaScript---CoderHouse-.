f7523cd0b7ba2af260cc010fd6073d3e6f98cd5a
lucasfcolman <106201646+lucasfcolman@users.noreply.github.com>
2022-08-13 15:47:58 -0300

    interactuando con htmla

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ let nameCourse = prompt ("Ingrese el nombre de la carrera que esta cursando: ")
 let carrera = document.getElementById ("Carrera")
 carrera.innerHTML = "Carrera: " + nameCourse;
  let mapaMaterias = document.getElementById("course");
- let map = map.materias (x =>{
+ let map = materias.map (x =>{
     return (`<div> ${x.id} </div>
              <h2> ${x.nombre} </h2>
-             <div>${x.Puntos} </div>`  )
+             <div>${x.Puntos} </div>`)
  })
 
-document.mapaMaterias.appendChild(map);
+mapaMaterias.innerHTML = map;
